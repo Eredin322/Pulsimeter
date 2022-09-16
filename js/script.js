bf435b02d1269e051cf36aa4b328f63e9a9235fb
Iluha <bkmrfrbkmrf326915@mail.ru>
2022-09-16 21:07:19 +0200

    Improved HTML code

--- js/script.js
+++ js/script.js
@@ -146,7 +146,7 @@ if(window.innerWidth < 730) {
     }
   });
 
-  $("a[href^='#'").click(function() {
+  $("a[href='#up'").click(function() {
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
